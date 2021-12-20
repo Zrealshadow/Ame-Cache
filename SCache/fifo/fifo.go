3915cf71dc1577930c67a5f01ab2e3fbefe005a0
Zreal <zenglz_pro@163.com>
2021-12-20 23:43:30 +0800

    fifo

---
+++ SCache/fifo/fifo.go
@@ -0,0 +1,101 @@
+package fifo
+
+import (
+	"container/list"
+
+	scache "github.com/lingze/localCache/SCache"
+)
+
+type fifo struct {
+	maxBytes int
+	// 缓存最大容量
+
+	onEvicted func(key string, value interface{})
+
+	usedBytes int
+
+	ll *list.List
+
+	cache map[string]*list.Element
+}
+
+func New(maxbytes int, onevicted func(key string, value interface{})) scache.Cache {
+	c := &fifo{
+		maxBytes:  maxbytes,
+		onEvicted: onevicted,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+	}
+	return c
+}
+
+// -------------- Interface --------------- //
+type entry struct {
+	key   string
+	value interface{}
+}
+
+func (e *entry) Len() int {
+	return scache.CalcLen(e.value)
+}
+
+func (c *fifo) Set(key string, value interface{}) {
+	if e, ok := c.cache[key]; ok {
+		c.ll.MoveToBack(e)
+		en := e.Value.(*entry)
+
+		//update size
+		c.usedBytes = c.usedBytes - scache.CalcLen(en.value) + scache.CalcLen(value)
+		en.value = value
+		return
+	}
+
+	// non-hit
+	en := &entry{key, value}
+	e := c.ll.PushBack(en)
+	c.cache[key] = e
+
+	c.usedBytes += en.Len()
+	if c.usedBytes > c.maxBytes {
+		c.DelOldest()
+		// Pop another record
+	}
+}
+
+func (c *fifo) Get(key string) interface{} {
+	if e, ok := c.cache[key]; ok {
+		return e.Value.(*entry).value
+	}
+	return nil
+}
+
+func (c *fifo) Del(key string) {
+	if e, ok := c.cache[key]; ok {
+		// the key exist
+		c.remove(e)
+	}
+}
+
+func (c *fifo) DelOldest() {
+	c.remove(c.ll.Front())
+}
+
+func (c *fifo) remove(e *list.Element) {
+	if e == nil {
+		return
+	}
+
+	c.ll.Remove(e)
+
+	en := e.Value.(*entry)
+	c.usedBytes = c.usedBytes - en.Len()
+	delete(c.cache, en.key)
+
+	if c.onEvicted != nil {
+		c.onEvicted(en.key, en.value)
+	}
+}
+
+func (c *fifo) Len() int {
+	return c.ll.Len()
+}
---
+++ SCache/fifo/fifo_test.go
@@ -0,0 +1,44 @@
+package fifo
+
+import (
+	"testing"
+
+	"github.com/matryer/is"
+)
+
+func TestSetGet(t *testing.T) {
+	is := is.New(t)
+
+	cache := New(24, nil)
+	cache.DelOldest()
+	cache.Set("k1", 1)
+	v := cache.Get("k1")
+	is.Equal(v, 1)
+
+	cache.Del("k1")
+	is.Equal(0, cache.Len()) // expect to be the same
+
+	// cache.Set("k2", time.Now())
+}
+
+func TestOnEvicted(t *testing.T) {
+	is := is.New(t)
+
+	keys := make([]string, 0, 8)
+	onEvicted := func(key string, value interface{}) {
+		keys = append(keys, key)
+	}
+	cache := New(16, onEvicted)
+
+	cache.Set("k1", 1)
+	cache.Set("k2", 2)
+	cache.Get("k1")
+	cache.Set("k3", 3)
+	cache.Get("k1")
+	cache.Set("k4", 4)
+
+	expected := []string{"k1", "k2"}
+
+	is.Equal(expected, keys)
+	is.Equal(2, cache.Len())
+}
